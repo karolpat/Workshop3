3b10b8c4660281261a228036a1b52ebefff193b0
Karol Patecki <karol.patecki09@gmail.com>
2017-12-29 12:14:17 +0100

    New servlet for the group of user page

--- src/warsztat/GroupPage.java
+++ src/warsztat/GroupPage.java
@@ -17,7 +17,7 @@ public class GroupPage extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		
+		getServletContext().getRequestDispatcher("/group.jsp").forward(request, response);		
 	}
 
 	
